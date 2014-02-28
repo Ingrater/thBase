e27f20038dc9bd92c60bdba3fb8c8c1f4c41895f
Benjamin Thaut <code@benjamin-thaut.de>
2014-02-28 13:02:59 +0100

    added copy module

---
+++ src/thBase/copy.d
@@ -0,0 +1,19 @@
+module thBase.copy;
+
+import thBase.traits;
+
+T shallowCopyFrom(T)(T to, T from)
+{
+  foreach(m; __traits(allMembers, T))
+  {
+    static if(m.length < 2 || m[0..2] != "__"){
+      static if(__traits(compiles,typeof(__traits(getMember, MT, m))))
+      {
+        static if(!isFunction!(typeof(__traits(getMember, to, m))))
+        {
+          __traits(getMember, to) = __traits(getMember, from);
+        }
+      }
+    }
+  }
+}
